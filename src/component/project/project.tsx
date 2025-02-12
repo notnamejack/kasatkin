7d903e5b7cf2e0d5641d403376e5e19cab09254a
Konstantinov Aleksey <KonstantinovAI@enplus.digital>
2025-02-12 10:13:12 +0800

    правки

--- src/component/project/project.tsx
+++ src/component/project/project.tsx
@@ -17,7 +17,7 @@ export function Project() {
 	const trendRef = useRef<HTMLDivElement>(null);
 	const turnRef = useRef<HTMLDivElement>(null);
 	const proRef = useRef<HTMLDivElement>(null);
-	const isMobile = window.matchMedia("(max-width: 1024px)").matches;
+	const isMobile = window.matchMedia('(max-width: 1024px)').matches;
 
 	useEffect(() => {
 		if (scroll >= startScroll)
@@ -38,165 +38,341 @@ export function Project() {
 
 	return (
 		<section className={s.project}>
-			{!isMobile && <div className={s.container_list}>
-				<div className={s.deactop_logo}>
-					<Logo />
-				</div>
-				<div className={s.list_project} ref={refProject}>
-					<div className={s.rigth}>
-						<a href='https://carmax.kasatkin.io/' target='_blank' title='carmax' className={clsx(s.container_image, s.carmax)}>
-							<div className={clsx(s.image)} ref={carmaxTRef}>
-								<video className={""} autoPlay loop muted playsInline
-									style={{width: '100%', height: '100%',transform: `scale(1.2) translateY(${(carmaxTRef?.current?.getBoundingClientRect()?.y || 100) * 0.01}px)`}}>
-									<source src='./video/car-max.mp4' type='video/mp4'/>
-								</video>
+			{!isMobile && (
+				<div className={s.container_list}>
+					<div className={s.deactop_logo}>
+						<Logo />
+					</div>
+					<div className={s.list_project} ref={refProject}>
+						<div className={s.rigth}>
+							<a
+								href='https://carmax.kasatkin.io/'
+								target='_blank'
+								title='carmax'
+								className={clsx(s.container_image, s.carmax)}>
+								<div className={clsx(s.image)} ref={carmaxTRef}>
+									<video
+										className={''}
+										autoPlay
+										loop
+										muted
+										playsInline
+										style={{
+											width: '100%',
+											height: '100%',
+											transform: `scale(1.2) translateY(${
+												(carmaxTRef?.current?.getBoundingClientRect()?.y ||
+													100) * 0.01
+											}px)`,
+										}}>
+										<source src='./video/car-max.mp4' type='video/mp4' />
+									</video>
+								</div>
+								<div className={s.image_name}>
+									<h3>Carmax</h3>
+								</div>
+							</a>
+							<a
+								href='https://pay.kasatkin.io'
+								target='_blank'
+								title='KASATKIN PAY'
+								className={clsx(s.container_image, s.turn)}>
+								<div className={clsx(s.image)} ref={turnRef}>
+									<video
+										className={''}
+										autoPlay
+										loop
+										muted
+										playsInline
+										style={{
+											width: '100%',
+											height: '100%',
+											transform: `scale(2) translateY(${
+												(turnRef?.current?.getBoundingClientRect()?.y || 100) *
+												0.01
+											}px) translatex(50px)`,
+										}}>
+										<source src='./video/turn.mp4' type='video/mp4' />
+									</video>
+								</div>
+								<div className={s.image_name}>
+									<h3>KASATKIN PAY</h3>
+								</div>
+							</a>
+							<div className={clsx(s.container_image, s.bank)}>
+								<div className={clsx(s.image)} ref={bankRef}>
+									<video
+										className={''}
+										autoPlay
+										loop
+										muted
+										playsInline
+										style={{
+											width: '100%',
+											height: '100%',
+											transform: `scale(2) translateY(${
+												(bankRef?.current?.getBoundingClientRect()?.y || 100) *
+												0.01
+											}px)`,
+										}}>
+										<source src='./video/bank.mp4' type='video/mp4' />
+									</video>
+								</div>
+								<div className={s.image_name}>
+									<h3>VAKKO WALLET</h3>
+								</div>
 							</div>
-							<div className={s.image_name}>
-								<h3>Carmax</h3>
-							</div>
-						</a>
-						<a href='https://pay.kasatkin.io' target='_blank' title='KASATKIN PAY' className={clsx(s.container_image, s.turn)}>
-							<div className={clsx(s.image)} ref={turnRef}>
-								<video className={""} autoPlay loop muted playsInline
-									style={{width: '100%', height: '100%',transform: `scale(2) translateY(${(turnRef?.current?.getBoundingClientRect()?.y || 100) * 0.01}px) translatex(50px)`}}>
-									<source src='./video/turn.mp4' type='video/mp4'/>
-								</video>
-							</div>
-							<div className={s.image_name}>
-								<h3>KASATKIN PAY</h3>
-							</div>
-						</a>
-						<div className={clsx(s.container_image, s.bank)}>
-							<div className={clsx(s.image)} ref={bankRef}>
-								<video className={""} autoPlay loop muted playsInline
-									style={{width: '100%', height: '100%',transform: `scale(2) translateY(${(bankRef?.current?.getBoundingClientRect()?.y || 100) * 0.01}px)`}}>
-									<source src='./video/bank.mp4' type='video/mp4'/>
-								</video>
-							</div>
-							<div className={s.image_name}>
-								<h3>VAKKO WALLET</h3>
-							</div>
+							<a
+								href='https://alpha-dex.io/'
+								target='_blank'
+								title='alpha dex'
+								className={clsx(s.container_image, s.trend)}>
+								<div className={clsx(s.image)} ref={trendRef}>
+									<video
+										className={''}
+										autoPlay
+										loop
+										muted
+										playsInline
+										style={{
+											width: '100%',
+											height: '100%',
+											transform: `scale(1.3) translateY(${
+												(trendRef?.current?.getBoundingClientRect()?.y || 100) *
+												0.01
+											}px)`,
+										}}>
+										<source src='./video/trend.mp4' type='video/mp4' />
+									</video>
+								</div>
+								<div className={s.image_name}>
+									<h3>aLPHA DEX</h3>
+								</div>
+							</a>
 						</div>
-						<a href='https://alpha-dex.io/' target='_blank' title='alpha dex' className={clsx(s.container_image, s.trend)}>
-							<div className={clsx(s.image)} ref={trendRef}>
-								<video className={""} autoPlay loop muted playsInline
-									style={{width: '100%', height: '100%',transform: `scale(1.3) translateY(${(trendRef?.current?.getBoundingClientRect()?.y || 100) * 0.01}px)`}}>
-									<source src='./video/trend.mp4' type='video/mp4'/>
-								</video>
-							</div>
-							<div className={s.image_name}>
-								<h3>aLPHA DEX</h3>
-							</div>
-						</a>
-					</div>
-					<div className={s.left}>
-						<a href='https://xpad.pro/' target='_blank' title='xpad pro' className={clsx(s.container_image, s.end)}>
-							<div className={clsx(s.image)} ref={endRef}>
-								<video className={""} autoPlay loop muted playsInline
-									style={{width: '100%', height: '100%',transform: `scale(1.3) translateY(${(endRef?.current?.getBoundingClientRect()?.y || 100) * 0.01}px)`}}>
-									<source src='./video/end.mp4' type='video/mp4'/>
-								</video>
-							</div>
-							<div className={s.image_name}>
-								<h3>xpad pro</h3>
-							</div>
-						</a>
-						<a href='https://carmax.kasatkin.io/' target='_blank' title='carmax' className={clsx(s.container_image, s.m_carmax)}>
-							<div className={clsx(s.image)} ref={carmaxRef} >
-								<video className={""} autoPlay loop muted playsInline
-									style={{width: '100%', height: '100%',transform: `scale(2) translateY(${(carmaxRef?.current?.getBoundingClientRect()?.y || 100) * 0.01}px)`}}>
-									<source src='./video/carmax.mp4' type='video/mp4'/>
-								</video>
-							</div>
-							<div className={s.image_name}>
-								<h3>Carmax</h3>
-							</div>
-						</a>
-						<a href='https://ushi.pro/' target='_blank' title='USHI'  className={clsx(s.container_image, s.gen)}>
-							<div className={clsx(s.image)} ref={gemRef}>
-								<video className={""} autoPlay loop muted playsInline
-									style={{width: '100%', height: '100%',transform: `scale(1.3) translateY(${(gemRef?.current?.getBoundingClientRect()?.y || 100) * 0.01}px)`}}>
-									<source src='./video/gem.mp4' type='video/mp4'/>
-								</video>
-							</div>
-							<div className={s.image_name}>
-								<h3>USHI</h3>
-							</div>
-						</a>
-						<a href='https://arkproduction.kasatkin.io/' target='_blank' title='ark production' className={clsx(s.container_image, s.video)}>
-							<div className={clsx(s.image)} ref={proRef}>
-								<video className={""} autoPlay loop muted playsInline
-									style={{width: '100%', height: '100%',transform: `scale(2) translateY(${(proRef?.current?.getBoundingClientRect()?.y || 100) * 0.01}px)`}}>
-									<source src='./video/pro.mp4' type='video/mp4'/>
-								</video>
-							</div>
-							<div className={s.image_name}>
-								<h3>ARK PRODUCTION</h3>
-							</div>
-						</a>
+						<div className={s.left}>
+							<a
+								href='https://xpad.pro/'
+								target='_blank'
+								title='xpad pro'
+								className={clsx(s.container_image, s.end)}>
+								<div className={clsx(s.image)} ref={endRef}>
+									<video
+										className={''}
+										autoPlay
+										loop
+										muted
+										playsInline
+										style={{
+											width: '100%',
+											height: '100%',
+											transform: `scale(1.3) translateY(${
+												(endRef?.current?.getBoundingClientRect()?.y || 100) *
+												0.01
+											}px)`,
+										}}>
+										<source src='./video/end.mp4' type='video/mp4' />
+									</video>
+								</div>
+								<div className={s.image_name}>
+									<h3>xpad pro</h3>
+								</div>
+							</a>
+							<a
+								href='https://carmax.kasatkin.io/'
+								target='_blank'
+								title='carmax'
+								className={clsx(s.container_image, s.m_carmax)}>
+								<div className={clsx(s.image)} ref={carmaxRef}>
+									<video
+										className={''}
+										autoPlay
+										loop
+										muted
+										playsInline
+										style={{
+											width: '100%',
+											height: '100%',
+											transform: `scale(2) translateY(${
+												(carmaxRef?.current?.getBoundingClientRect()?.y ||
+													100) * 0.01
+											}px)`,
+										}}>
+										<source src='./video/carmax.mp4' type='video/mp4' />
+									</video>
+								</div>
+								<div className={s.image_name}>
+									<h3>Carmax</h3>
+								</div>
+							</a>
+							<a
+								href='https://ushi.pro/'
+								target='_blank'
+								title='USHI'
+								className={clsx(s.container_image, s.gen)}>
+								<div className={clsx(s.image)} ref={gemRef}>
+									<video
+										className={''}
+										autoPlay
+										loop
+										muted
+										playsInline
+										style={{
+											width: '100%',
+											height: '100%',
+											transform: `scale(1.3) translateY(${
+												(gemRef?.current?.getBoundingClientRect()?.y || 100) *
+												0.01
+											}px)`,
+										}}>
+										<source src='./video/gem.mp4' type='video/mp4' />
+									</video>
+								</div>
+								<div className={s.image_name}>
+									<h3>USHI</h3>
+								</div>
+							</a>
+							<a
+								href='https://arkproduction.kasatkin.io/'
+								target='_blank'
+								title='ark production'
+								className={clsx(s.container_image, s.video)}>
+								<div className={clsx(s.image)} ref={proRef}>
+									<video
+										className={''}
+										autoPlay
+										loop
+										muted
+										playsInline
+										style={{
+											width: '100%',
+											height: '100%',
+											transform: `scale(2) translateY(${
+												(proRef?.current?.getBoundingClientRect()?.y || 100) *
+												0.01
+											}px)`,
+										}}>
+										<source src='./video/pro.mp4' type='video/mp4' />
+									</video>
+								</div>
+								<div className={s.image_name}>
+									<h3>ARK PRODUCTION</h3>
+								</div>
+							</a>
+						</div>
 					</div>
 				</div>
-			</div>}
+			)}
 			<div className={s.after}></div>
-			{isMobile &&
-			<div className={s.container_mobile}>
-				<h2>Портфолио</h2>
-				<div className={s.image_modile}>
-					<a href='https://carmax.kasatkin.io/' target='_blank' title='carmax' className={clsx(s.carmax, s.image)}>
-						<video className={""} autoPlay loop muted playsInline
-							style={{width: '100%', height: '100%'}}>
-							<source src='./video/carmax.mp4' type='video/mp4'/>
-						</video>
-					</a>
-					<h3>Carmax</h3>
-				</div>
-				<div className={s.image_modile}>
-					<a href='https://xpad.pro/' target='_blank' title='xpad pro'  className={clsx(s.carmax, s.image)}>
-						<video className={""} autoPlay loop muted playsInline
-							style={{width: '100%', height: '100%'}}>
-							<source src='./video/end.mp4' type='video/mp4'/>
-						</video>
-					</a>
-					<h3>xpad pro</h3>
-				</div>
-				<div className={s.image_modile}>
-					<a href='https://pay.kasatkin.io' target='_blank' title='KASATKIN PAY' className={clsx(s.carmax, s.image)} >
-						<video className={""} autoPlay loop muted playsInline
-							style={{width: '100%', height: '100%'}}>
-							<source src='./video/turn.mp4' type='video/mp4'/>
-						</video>
-					</a>
-					<h3>KASATKIN PAY</h3>
-				</div>
-				<div className={s.image_modile}>
-					<div className={clsx(s.carmax, s.image)} >
-						<video className={""} autoPlay loop muted playsInline
-							style={{width: '100%', height: '100%'}}>
-							<source src='./video/bank.mp4' type='video/mp4'/>
-						</video>
+			{isMobile && (
+				<div className={s.container_mobile}>
+					<h2>Портфолио</h2>
+					<div className={s.image_modile}>
+						<a
+							href='https://carmax.kasatkin.io/'
+							target='_blank'
+							title='carmax'
+							className={clsx(s.carmax, s.image)}>
+							<video
+								className={''}
+								autoPlay
+								loop
+								muted
+								playsInline
+								style={{ width: '100%', height: '100%' }}>
+								<source src='./video/carmax.mp4' type='video/mp4' />
+							</video>
+						</a>
+						<h3>Carmax</h3>
 					</div>
-					<h3>VAKKO WALLET</h3>
+					<div className={s.image_modile}>
+						<a
+							href='https://xpad.pro/'
+							target='_blank'
+							title='xpad pro'
+							className={clsx(s.carmax, s.image)}>
+							<video
+								className={''}
+								autoPlay
+								loop
+								muted
+								playsInline
+								style={{ width: '100%', height: '100%' }}>
+								<source src='./video/end.mp4' type='video/mp4' />
+							</video>
+						</a>
+						<h3>xpad pro</h3>
+					</div>
+					<div className={s.image_modile}>
+						<a
+							href='https://pay.kasatkin.io'
+							target='_blank'
+							title='KASATKIN PAY'
+							className={clsx(s.carmax, s.image)}>
+							<video
+								className={''}
+								autoPlay
+								loop
+								muted
+								playsInline
+								style={{ width: '100%', height: '100%' }}>
+								<source src='./video/turn.mp4' type='video/mp4' />
+							</video>
+						</a>
+						<h3>KASATKIN PAY</h3>
+					</div>
+					<div className={s.image_modile}>
+						<div className={clsx(s.carmax, s.image)}>
+							<video
+								className={''}
+								autoPlay
+								loop
+								muted
+								playsInline
+								style={{ width: '100%', height: '100%' }}>
+								<source src='./video/bank.mp4' type='video/mp4' />
+							</video>
+						</div>
+						<h3>VAKKO WALLET</h3>
+					</div>
+					<div className={s.image_modile}>
+						<a
+							href='https://alpha-dex.io/'
+							target='_blank'
+							title='alpha dex'
+							className={clsx(s.carmax, s.image)}>
+							<video
+								className={''}
+								autoPlay
+								loop
+								muted
+								playsInline
+								style={{ width: '100%', height: '100%' }}>
+								<source src='./video/trend.mp4' type='video/mp4' />
+							</video>
+						</a>
+						<h3>alpha dex</h3>
+					</div>
+					<div className={s.image_modile}>
+						<a
+							href='https://arkproduction.kasatkin.io/'
+							target='_blank'
+							title='ark production'
+							className={clsx(s.carmax, s.image)}>
+							<video
+								className={''}
+								autoPlay
+								loop
+								muted
+								playsInline
+								style={{ width: '100%', height: '100%' }}>
+								<source src='./video/pro.mp4' type='video/mp4' />
+							</video>
+						</a>
+						<h3>ARK PRODUCTION</h3>
+					</div>
 				</div>
-				<div className={s.image_modile}>
-					<a href='https://alpha-dex.io/' target='_blank' title='alpha dex' className={clsx(s.carmax, s.image)} >
-						<video className={""} autoPlay loop muted playsInline
-							style={{width: '100%', height: '100%'}}>
-							<source src='./video/trend.mp4' type='video/mp4'/>
-						</video>
-					</a>
-					<h3>alpha dex</h3>
-				</div>
-				<div className={s.image_modile}>
-					<a href='https://arkproduction.kasatkin.io/' target='_blank' title='ark production' className={clsx(s.carmax, s.image)} >
-						<video className={""} autoPlay loop muted playsInline
-							style={{width: '100%', height: '100%'}}>
-							<source src='./video/pro.mp4' type='video/mp4'/>
-						</video>
-					</a>
-					<h3>ARK PRODUCTION</h3>
-				</div>
-			</div>
-			}
+			)}
 		</section>
 	);
 }
